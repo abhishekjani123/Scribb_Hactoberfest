9febb5a91aa5e9a77351d202953bd50ab8216de7
sneh20122001 <95904935+sneh20122001@users.noreply.github.com>
2022-10-31 12:41:47 +0530

    Create s1.py

---
+++ s1.py
@@ -0,0 +1,11 @@
+import math
+ 
+def fact(t):
+    for x in range(t):
+        n = int(input())
+        print(math.factorial(n))
+ 
+ 
+if __name__ == '__main__':
+    ans = int(input())
+    fact(ans) 
