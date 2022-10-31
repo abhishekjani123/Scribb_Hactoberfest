61437937493c37d493858b314d5d5c2b7c382685
HARIOM26 <72153454+HARIOM26@users.noreply.github.com>
2022-10-31 23:15:14 +0530

    Create hom.py

---
+++ hom.py
@@ -0,0 +1,14 @@
+a=2
+b='dax'
+c=2.45
+d=313.6578
+e='x'
+f='12'
+g= b
+print("type of %d"%a,type(a))
+print("type of "+b,type(b))
+print("type of %d"%c,type(c))
+print("type of %d"%d,type(d))
+print("type of "+e,type(e))
+print("type of "+f,type(f))
+print("type of "+g,type(g))
